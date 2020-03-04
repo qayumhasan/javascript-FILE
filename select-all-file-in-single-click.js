440c3488d7fde0d6857a71f1b744c06889e3acd8
Qayum Hasan <45765971+qayumhasan@users.noreply.github.com>
2020-03-04 17:02:42 +0600

    Create select-all-file-in-single-click.js

---
+++ select-all-file-in-single-click.js
@@ -0,0 +1,16 @@
+<script type="text/javascript">
+
+    $(document).ready(function () {
+
+        $('#check_all').on('click', function (e) {
+
+            if ($(this).is(':checked', true)) {
+                $(".checkbox").prop('checked', true);
+            } else {
+                $(".checkbox").prop('checked', false);
+            }
+        });
+
+    });
+
+</script>
